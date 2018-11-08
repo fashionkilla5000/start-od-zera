88da63607df92cd383fcb1ae5a550f6dd3bb8c8b
fashionkilla5000 <44493009+fashionkilla5000@users.noreply.github.com>
2018-11-08 16:37:50 +0100

    program

---
+++ program.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+
+    do
+    {
+       printf("podaj liczbe od 1 do 100\n");
+       scanf("%i",&x);
+
+    }while(!(x>=0 || x<=100));
+
+     printf("podano liczbe wieksza od 0 i mniejsza od 100");
+
+
+    getchar();
+    return 0;
+
+
+}
